86929a1ee1ef081e9643a6841ca0420dc67e577c
anishch <43155397+anishch@users.noreply.github.com>
2021-11-08 01:33:15 -0800

    comments for clarity in WeightSystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/WeightSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/WeightSystem.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.components;
 
 public class WeightSystem {
+    // what was the thing I tested? ask David and copy code in - apply same logic as States used for Tflow
 }
