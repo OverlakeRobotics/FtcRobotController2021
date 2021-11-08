9865e6934ce04d3f9e01ff240ac69287a4e81ccc
anishch <43155397+anishch@users.noreply.github.com>
2021-11-08 15:39:03 -0800

    WeightSystem additions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/WeightSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/WeightSystem.java
@@ -1,5 +1,32 @@
 package org.firstinspires.ftc.teamcode.components;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-public class WeightSystem {
-    // what was the thing I tested? ask David and copy code in - apply same logic as States used for Tflow
+public class WeightSystem extends LinearOpMode {
+
+  private AnalogInput sensorAsAnalogInput;
+
+  /*
+   * This function is executed when this Op Mode is selected from the Driver Station.
+   */
+
+
+@Override
+public void runOpMode() {
+    sensorAsAnalogInput = hardwareMap.get(AnalogInput.class, "sensorAsAnalogInput");
+
+    // Configure digital pin for input mode.
+    telemetry.addData("DigitalTouchSensorExample", "Press start to continue...");
+    telemetry.update();
+    waitForStart();
+    if (opModeIsActive()) {
+        // Put run blocks here.
+        while (opModeIsActive()) {
+            // button is pressed if value returned is LOW or false.
+            telemetry.addData("Voltage", sensorAsAnalogInput.getVoltage());
+            telemetry.update();
+        }
+    }
 }
+}
+
