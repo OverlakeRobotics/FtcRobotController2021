23c90af0b39cee123e078781fce6e7ce37d4f8e3
Jack <jackslater1@gmail.com>
2021-11-22 16:47:48 -0800

    made some more good changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/AutonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/AutonomousOpMode.java
@@ -6,7 +6,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.components.ArmSystem;
 import org.firstinspires.ftc.teamcode.components.DriveSystem;
 import org.firstinspires.ftc.teamcode.components.IntakeSystem;
+import org.firstinspires.ftc.teamcode.components.TensorFlow;
+import org.firstinspires.ftc.teamcode.components.TensorFlowNew;
 import org.firstinspires.ftc.teamcode.components.TurnTableSystem;
+import org.firstinspires.ftc.teamcode.components.Vuforia;
 import org.firstinspires.ftc.teamcode.helpers.Constants;
 
 import org.firstinspires.ftc.teamcode.helpers.GameState;
@@ -18,57 +21,35 @@ public abstract class AutonomousOpMode extends BaseOpMode {
 
 
     private int elevatorLevel;
-    boolean[] barcodes;
+    private boolean isRBorBT;
 
     private GameState currentGameState;
     private RouteState currentRouteState;
     protected TeamState teamState;
 
-    private int level;
-
     private static final double driveSpeed = 0.5;
     private static final double rotateSpeed = 0.25;
 
     // Systems
-    //private TensorFlow tensorflow;
-    //private TensorFlowNew tensorflowNew;
-    //private Vuforia vuforia;
+    private TensorFlow tensorflow;
+    private TensorFlowNew tensorflowNew;
+    private Vuforia vuforia;
 
     public void init(TeamState teamState, RouteState routeState) {
+        isRBorBT = ((teamState == TeamState.BLUE && currentRouteState == RouteState.TOP) || (teamState == TeamState.RED && currentRouteState == RouteState.BOTTOM));
         super.init();
         this.teamState = teamState;
         this.currentRouteState = routeState;
-        newGameState(GameState.INITIAL);
+        newGameState(GameState.SCAN_INITIAL);
         driveSystem.initMotors();
-        //vuforia = Vuforia.getInstance();
-        //tensorflow = new TensorFlow(vuforia);
-        //tensorflowNew = new TensorFlowNew(vuforia);
+        vuforia = Vuforia.getInstance();
+        tensorflow = new TensorFlow(vuforia);
+        tensorflowNew = new TensorFlowNew(vuforia);
         armSystem = new ArmSystem(hardwareMap.get(DcMotor.class, Constants.ELEVATOR_MOTOR));
         armSystem.initMotors();
         intakeSystem = new IntakeSystem(hardwareMap.get(DcMotor.class, Constants.INTAKE_MOTOR1), hardwareMap.get(DcMotor.class, Constants.INTAKE_MOTOR2));
         intakeSystem.initMotors();
         turnTableSystem = new TurnTableSystem(hardwareMap.get(DcMotor.class, Constants.ROTATOR_MOTOR));
-        barcodes = new boolean[3];
-        /*for (int x = 0; x <= 2; x++) {
-            tensorflowNew.activate(x);
-            barcodes[x] = (tensorflowNew.getInference().size() > 0);
-            if (barcodes[x]) {
-                elevatorLevel = tensorflowNew.getObjectNew(x);
-                break;
-            }
-            else if (!barcodes[x] && x == 2){
-                double giveUpHope = Math.random();
-                if (giveUpHope < 0.33) {
-                    elevatorLevel = ArmSystem.LEVEL_BOTTOM;
-                }
-                if (giveUpHope < 0.66) {
-                    elevatorLevel =  ArmSystem.LEVEL_MID;
-                }
-                else {
-                    elevatorLevel = ArmSystem.LEVEL_TOP; //TODO: If not detected in center position, create states for moving left and right, also put camera on left side, mounted so that the camera detects the duck in the provided space
-                }
-            }
-        }*/
     }
 
     @Override
@@ -77,7 +58,7 @@ public abstract class AutonomousOpMode extends BaseOpMode {
         //vuforia.activate();
         //tensorflow.activate()
 
-        newGameState(GameState.DRIVE_TO_ALLIANCE_HUB_ONE);
+        newGameState(GameState.SCAN_INITIAL);
     }
 
     @Override
@@ -85,27 +66,75 @@ public abstract class AutonomousOpMode extends BaseOpMode {
         telemetry.addData("GameState", currentGameState);
 
         switch (currentGameState) {
-            case DRIVE_TO_ALLIANCE_HUB_ONE:
-                if (driveSystem.driveToPosition(300, DriveSystem.Direction.LEFT, driveSpeed)) {
+            case SCAN_INITIAL:
+                if (tensorflowNew.getInference().size() > 0) {
+                    elevatorLevel = ArmSystem.LEVEL_MID;
+                    newGameState(GameState.DRIVE_TO_ALLIANCE_HUB_ONE_PRIMARY);
+                } else {
+                    newGameState(GameState.SCAN_SECONDARY);
+                }
+                break;
+            case SCAN_SECONDARY:
+                if (driveSystem.driveToPosition((int) (Constants.tileWidth * Constants.mmPerInch * (2/3)), isRBorBT ? DriveSystem.Direction.FORWARD : DriveSystem.Direction.BACKWARD, driveSpeed)) {
+                    if (tensorflowNew.getInference().size() > 0) {
+                        elevatorLevel = isRBorBT ? ArmSystem.LEVEL_BOTTOM : ArmSystem.LEVEL_TOP;
+                        newGameState(GameState.DRIVE_TO_ALLIANCE_HUB_ONE_PRIMARY);
+                    } else {
+                        elevatorLevel = isRBorBT ? ArmSystem.LEVEL_TOP : ArmSystem.LEVEL_BOTTOM;
+                    }
+                    newGameState(GameState.DRIVE_TO_ALLIANCE_HUB_ONE_SECONDARY);
+                }
+                break;
+            case DRIVE_TO_ALLIANCE_HUB_ONE_PRIMARY:
+                if (driveSystem.driveToPosition((int) (Constants.tileWidth * Constants.mmPerInch), isRBorBT ? DriveSystem.Direction.FORWARD : DriveSystem.Direction.BACKWARD, driveSpeed)) {
+                    newGameState(GameState.DRIVE_TO_ALLIANCE_HUB_TWO);
+                }
+                break;
+            case DRIVE_TO_ALLIANCE_HUB_ONE_SECONDARY:
+                if (driveSystem.driveToPosition((int) (Constants.tileWidth * Constants.mmPerInch * (1/3)), isRBorBT ? DriveSystem.Direction.FORWARD : DriveSystem.Direction.BACKWARD, driveSpeed)) {
                     newGameState(GameState.DRIVE_TO_ALLIANCE_HUB_TWO);
                 }
                 break;
             case DRIVE_TO_ALLIANCE_HUB_TWO:
-                if (driveSystem.driveToPosition(300, DriveSystem.Direction.BACKWARD, driveSpeed)) {
+                if (driveSystem.driveToPosition((int) (Constants.tileWidth * (4/7) * Constants.mmPerInch), DriveSystem.Direction.LEFT, driveSpeed)) {
                     newGameState(GameState.DRIVE_TO_ALLIANCE_HUB_THREE);
                 }
                 break;
             case DRIVE_TO_ALLIANCE_HUB_THREE:
-                if (driveSystem.turn(-180, rotateSpeed)) {
-                    newGameState(GameState.PLACE_CUBE);
+                if (driveSystem.turn(90, rotateSpeed)) {
+                    newGameState(GameState.PLACE_CUBE_ONE);
                 }
                 break;
 
-            case PLACE_CUBE:
+            case PLACE_CUBE_ONE:
+                if (elevatorLevel == 0){
+                    double giveUpHope = Math.random();
+                    if (giveUpHope < 0.33) {
+                        elevatorLevel = ArmSystem.LEVEL_BOTTOM;
+                    }
+                    if (giveUpHope < 0.66) {
+                        elevatorLevel =  ArmSystem.LEVEL_MID;
+                    }
+                    else {
+                        elevatorLevel = ArmSystem.LEVEL_TOP;
+                    }
+                }
                 armSystem.goToLevel(elevatorLevel);
                 intakeSystem.spit_out();
                 armSystem.goToLevel(ArmSystem.LEVEL_BOTTOM);
-                newGameState(currentRouteState == RouteState.TOP ? GameState.PARK_IN_WAREHOUSE_ONE : GameState.DRIVE_TO_CAROUSEL_ONE);
+                newGameState(GameState.PLACE_CUBE_TWO);
+                break;
+
+            case PLACE_CUBE_TWO:
+                if (driveSystem.turn(isRBorBT ? 90 : -90, rotateSpeed)) {
+                    newGameState(GameState.PLACE_CUBE_THREE);
+                }
+                break;
+
+            case PLACE_CUBE_THREE:
+                if (driveSystem.driveToPosition((int) (Constants.tileWidth * (3/5) * Constants.mmPerInch), isRBorBT ? DriveSystem.Direction.LEFT : DriveSystem.Direction.RIGHT, driveSpeed)) {
+                    newGameState(currentRouteState == RouteState.TOP ? GameState.PARK_IN_WAREHOUSE_ONE : GameState.DRIVE_TO_CAROUSEL_ONE);
+                }
                 break;
 
             case DRIVE_TO_CAROUSEL_ONE:
@@ -146,12 +175,18 @@ public abstract class AutonomousOpMode extends BaseOpMode {
                 break;
 
             case PARK_IN_WAREHOUSE_ONE:
-                if (driveSystem.driveToPosition(350, DriveSystem.Direction.LEFT, driveSpeed)) {
+                if (driveSystem.turn(teamState == TeamState.RED ? -90 : 90, rotateSpeed)) {
                     newGameState(GameState.PARK_IN_WAREHOUSE_TWO);
                 }
                 break;
 
             case PARK_IN_WAREHOUSE_TWO:
+                if (driveSystem.driveToPosition(350, DriveSystem.Direction.LEFT, driveSpeed)) {
+                    newGameState(GameState.PARK_IN_WAREHOUSE_TWO);
+                }
+                break;
+
+            case PARK_IN_WAREHOUSE_THREE:
                 if (driveSystem.driveToPosition(1000, DriveSystem.Direction.BACKWARD, driveSpeed)) {
                     newGameState(GameState.COMPLETE);
                 }
