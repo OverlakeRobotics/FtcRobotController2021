a3251bd546ec63220078d8aafedc5ae367962e2e
anishch <43155397+anishch@users.noreply.github.com>
2021-12-10 14:04:41 -0800

    added more methods ArmSystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/ArmSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/ArmSystem.java
@@ -87,17 +87,20 @@ public class ArmSystem {
      * Moves arm up
      */
     public void move_up() {
-        elevatorMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        elevatorMotor.setPower(1.0);
-
+        if (notTooHigh()){
+            elevatorMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            elevatorMotor.setPower(1.0);
+        }
     }
 
     /**
      * Moves arm down
      */
     public void move_down() {
-        elevatorMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        elevatorMotor.setPower(-0.75);
+        if (notTooLow()){
+            elevatorMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            elevatorMotor.setPower(-0.75);
+        }
     }
 
     public void moveToPosition(int ticks){
